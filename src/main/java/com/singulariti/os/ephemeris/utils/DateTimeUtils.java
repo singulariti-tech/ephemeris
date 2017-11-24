02999892a021a89574641628438ac32c805c6b9d
John Emmanuel <johnemmanueljd@gmail.com>
2017-11-24 11:04:36 +0530

    MODIFIED: Added license information

--- src/main/java/com/singulariti/os/ephemeris/utils/DateTimeUtils.java
+++ src/main/java/com/singulariti/os/ephemeris/utils/DateTimeUtils.java
@@ -1,3 +1,19 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.singulariti.os.ephemeris.utils;
 
 import com.singulariti.os.ephemeris.domain.Observatory;
@@ -59,9 +75,9 @@ public class DateTimeUtils {
         return TimeZone.getDefault().inDaylightTime(new Date());
     }
 
-    public static double jd(Observatory obs){
-        double jd0 = jd0(obs.getFullYear(),obs.getMonth(),obs.getDate());
-        jd0 += (obs.getHours()+((obs.getMinutes()+obs.getTimeDifferenceGMTMinutes())/60.0)+(obs.getSeconds()/3600.0))/24;
+    public static double jd(Observatory obs) {
+        double jd0 = jd0(obs.getFullYear(), obs.getMonth(), obs.getDate());
+        jd0 += (obs.getHours() + ((obs.getMinutes() + obs.getTimeDifferenceGMTMinutes()) / 60.0) + (obs.getSeconds() / 3600.0)) / 24;
         return jd0;
     }
 
